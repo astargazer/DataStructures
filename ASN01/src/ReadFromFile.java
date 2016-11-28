2d516b0ded2ed314a96642f49e00e56398f89a0e
Anna Mikesell Grossman <ammikesell@gmail.com>
2016-11-27 21:33:06 -0500

    added comments

--- ASN01/src/ReadFromFile.java
+++ ASN01/src/ReadFromFile.java
@@ -10,15 +10,17 @@ import java.io.IOException;
  */
 public class ReadFromFile {
 
-	private static final String FILENAME ="testfile.txt";
+	private static final String FILENAME ="testfile.txt";// the name of the file to be read from the local directory
 	public static void main(String[] args) {
 		BufferedReader br = null;
 		try {
 			File f = new File(FILENAME);
-			br = new BufferedReader(new FileReader(f));
+			br = new BufferedReader(new FileReader(f));//creates new reader to read from the file
+			//may throw a FNF exception
 			String line;
-			while((line = br.readLine())!=null){
-				System.out.println(line);
+			while((line = br.readLine())!=null){ //assigns the next available line to the line String variable until end of file
+				//may throw IO exception
+				System.out.println(line);//writes the line to console
 			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -27,7 +29,8 @@ public class ReadFromFile {
 		}
 		finally{
 			try {
-				br.close();
+				br.close(); // buffered readers and writers must be closed after use
+				//may throw IO exception
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
